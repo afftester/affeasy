e33e1b5ef25a67a2e74faef5db8e9cae816eea21
rfrlcode <122701660+rfrlcode@users.noreply.github.com>
2024-08-18 11:08:22 -0700

    trying to see if email works

--- apps/web/app/api/auth/account-exists/route.ts
+++ apps/web/app/api/auth/account-exists/route.ts
@@ -1,16 +1,14 @@
-import { getIdentityHash } from "@/lib/edge";
 import { isWhitelistedEmail } from "@/lib/edge-config";
 import { DATABASE_URL, conn } from "@/lib/planetscale";
 import { ratelimit } from "@/lib/upstash";
+import { ipAddress } from "@vercel/edge";
 import { NextRequest, NextResponse } from "next/server";
 
 export const runtime = "edge";
 
 export async function POST(req: NextRequest) {
-  const identity_hash = await getIdentityHash(req);
-  const { success } = await ratelimit(5, "1 m").limit(
-    `account-exists:${identity_hash}`,
-  );
+  const ip = ipAddress(req);
+  const { success } = await ratelimit(8, "1 m").limit(`account-exists:${ip}`);
   if (!success) {
     return new Response("Don't DDoS me pls 🥺", { status: 429 });
   }
@@ -24,21 +22,27 @@ export async function POST(req: NextRequest) {
   }
 
   if (!process.env.NEXT_PUBLIC_IS_DUB) {
-    return NextResponse.json({ exists: true });
+    return NextResponse.json({ accountExists: true, hasPassword: true });
   }
 
   const user = await conn
-    .execute("SELECT email FROM User WHERE email = ?", [email])
+    .execute("SELECT email, passwordHash FROM User WHERE email = ?", [email])
     .then((res) => res.rows[0]);
 
   if (user) {
-    return NextResponse.json({ exists: true });
+    return NextResponse.json({
+      accountExists: true,
+      hasPassword: !!user.passwordHash,
+    });
   }
 
   const whitelisted = await isWhitelistedEmail(email);
   if (whitelisted) {
-    return NextResponse.json({ exists: true });
+    return NextResponse.json({
+      accountExists: true,
+      hasPassword: false,
+    });
   }
 
-  return NextResponse.json({ exists: false });
+  return NextResponse.json({ accountExists: false, hasPassword: false });
 }
--- apps/web/emails/index.ts
+++ apps/web/emails/index.ts
@@ -2,12 +2,16 @@ import { render } from "@react-email/render";
 import { Client } from "postmark";
 import { JSXElementConstructor, ReactElement } from "react";
 
-export const client = new Client(process.env.POSTMARK_API_KEY as string);
+export const client = process.env.POSTMARK_API_KEY
+  ? new Client(process.env.POSTMARK_API_KEY)
+  : null;
 
 export const sendEmail = async ({
   email,
   subject,
   from,
+  bcc,
+  replyToFromEmail,
   text,
   react,
   marketing,
@@ -15,12 +19,22 @@ export const sendEmail = async ({
   email: string;
   subject: string;
   from?: string;
+  bcc?: string;
+  replyToFromEmail?: boolean;
   text?: string;
   react?: ReactElement<any, string | JSXElementConstructor<any>>;
   marketing?: boolean;
 }) => {
-  if (!process.env.POSTMARK_API_KEY) {
-    console.log(
+  if (process.env.NODE_ENV === "development" && !client) {
+    // Set up a fake email client for development
+    console.info(
+      `Email to ${email} with subject ${subject} sent from ${
+        from || process.env.NEXT_PUBLIC_APP_NAME
+      }`,
+    );
+    return Promise.resolve();
+  } else if (!client) {
+    console.error(
       "Postmark is not configured. You need to add a POSTMARK_API_KEY in your .env file for emails to work.",
     );
     return Promise.resolve();
@@ -28,12 +42,13 @@ export const sendEmail = async ({
 
   return client.sendEmail({
     From:
-      from || marketing
-        ? "steven@ship.dub.co"
-        : process.env.NEXT_PUBLIC_IS_DUB
-          ? "system@dub.co"
-          : `${process.env.NEXT_PUBLIC_APP_NAME} <system@${process.env.NEXT_PUBLIC_APP_DOMAIN}>`,
+      from ||
+      (marketing ? "ritanshu@ship.affeasy.link" : "system@notify.affeasy.link"),
     To: email,
+    Bcc: bcc,
+    ...(!replyToFromEmail && {
+      ReplyTo: "support@affeasy.link",
+    }),
     Subject: subject,
     ...(text && { TextBody: text }),
     ...(react && { HtmlBody: render(react) }),
