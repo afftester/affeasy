5fdc4d08624ff796af2c33afeb76d7500eabb378
rfrlcode <122701660+rfrlcode@users.noreply.github.com>
2024-09-08 11:14:55 -0700

    fixed login and register pages

--- apps/web/app/app.affeasy.link/(auth)/login/form.tsx
+++ apps/web/app/app.affeasy.link/(auth)/login/form.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button, Github, Google, InfoTooltip, useMediaQuery } from "@dub/ui";
+import { Button, Github, Google, useMediaQuery } from "@dub/ui";
 import { signIn } from "next-auth/react";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
@@ -38,7 +38,7 @@ export default function LoginForm() {
 
   return (
     <>
-      <div className="flex space-x-2">
+      <div className="flex flex-col space-y-3">
         <Button
           variant="secondary"
           onClick={() => {
@@ -64,158 +64,16 @@ export default function LoginForm() {
           icon={<Github className="h-5 w-5 text-black" />}
         />
       </div>
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
-          setClickedEmail(true);
-          fetch("/api/auth/account-exists", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ email }),
-          })
-            .then(async (res) => {
-              const { exists } = await res.json();
-              if (exists) {
-                signIn("email", {
-                  email,
-                  redirect: false,
-                  ...(next && next.length > 0 ? { callbackUrl: next } : {}),
-                }).then((res) => {
-                  setClickedEmail(false);
-                  if (res?.ok && !res?.error) {
-                    setEmail("");
-                    toast.success("Email sent - check your inbox!");
-                  } else {
-                    toast.error("Error sending email - try again?");
-                  }
-                });
-              } else {
-                toast.error("No account found with that email address.");
-                setNoSuchAccount(true);
-                setClickedEmail(false);
-              }
-            })
-            .catch(() => {
-              setClickedEmail(false);
-              toast.error("Error sending email - try again?");
-            });
-        }}
-        className="flex flex-col space-y-3"
-      >
-        {showEmailOption && (
-          <div>
-            <div className="mb-4 mt-1 border-t border-gray-300" />
-            <input
-              id="email"
-              name="email"
-              autoFocus={!isMobile}
-              type="email"
-              placeholder="easy@affiliate.life"
-              autoComplete="email"
-              required
-              value={email}
-              onChange={(e) => {
-                setNoSuchAccount(false);
-                setEmail(e.target.value);
-              }}
-              className="mt-1 block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
-            />
-          </div>
-        )}
-        <Button
-          text="Continue with Email"
-          variant="secondary"
-          {...(!showEmailOption && {
-            type: "button",
-            onClick: (e) => {
-              e.preventDefault();
-              setShowSSOOption(false);
-              setShowEmailOption(true);
-            },
-          })}
-          loading={clickedEmail}
-          disabled={clickedGoogle || clickedSSO}
-        />
-      </form>
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
-          setClickedSSO(true);
-          fetch("/api/auth/saml/verify", {
-            method: "POST",
-            body: JSON.stringify({ slug: e.currentTarget.slug.value }),
-          }).then(async (res) => {
-            const { data, error } = await res.json();
-            if (error) {
-              toast.error(error);
-              setClickedSSO(false);
-              return;
-            }
-            await signIn("saml", undefined, {
-              tenant: data.workspaceId,
-              product: "Dub",
-            });
-          });
-        }}
-        className="flex flex-col space-y-3"
-      >
-        {showSSOOption && (
-          <div>
-            <div className="mb-4 mt-1 border-t border-gray-300" />
-            <div className="flex items-center space-x-2">
-              <h2 className="text-sm font-medium text-gray-900">
-                Workspace Slug
-              </h2>
-              <InfoTooltip
-                content={`This is your workspace's unique identifier on ${process.env.NEXT_PUBLIC_APP_NAME}. E.g. app.affeasy.link/acme is "acme".`}
-              />
-            </div>
-            <input
-              id="slug"
-              name="slug"
-              autoFocus={!isMobile}
-              type="text"
-              placeholder="my-team"
-              autoComplete="off"
-              required
-              className="mt-1 block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
-            />
-          </div>
-        )}
-        {/* <Button
-          text="Continue with SAML SSO"
-          variant="secondary"
-          {...(!showSSOOption && {
-            type: "button",
-            onClick: (e) => {
-              e.preventDefault();
-              setShowEmailOption(false);
-              setShowSSOOption(true);
-            },
-          })}
-          loading={clickedSSO}
-          disabled={clickedGoogle || clickedEmail}
-        /> */}
-      </form>
-      {noSuchAccount ? (
-        <p className="text-center text-sm text-red-500">
-          No such account.{" "}
-          <Link href="/register" className="font-semibold text-red-600">
-            Sign up
-          </Link>{" "}
-          instead?
-        </p>
-      ) : (
-        <p className="text-center text-sm text-gray-500">
-          Don't have an account?{" "}
-          <Link
-            href="/register"
-            className="font-semibold text-gray-500 transition-colors hover:text-black"
-          >
-            Sign up
-          </Link>
-        </p>
-      )}
+
+      <p className="text-center text-sm text-gray-500">
+        Don't have an account?{" "}
+        <Link
+          href="/register"
+          className="font-semibold text-gray-500 transition-colors hover:text-black"
+        >
+          Sign up
+        </Link>
+      </p>
     </>
   );
 }
