0657d878bd391d3b018d44de7b1a7ee2618eb715
rfrlcode <122701660+rfrlcode@users.noreply.github.com>
2024-09-12 09:06:58 -0700

    used own video

--- apps/web/app/marketing/marketing-components/VideoPreview.tsx
+++ apps/web/app/marketing/marketing-components/VideoPreview.tsx
@@ -1,18 +1,6 @@
 "use client";
-import { useState } from "react";
-
-const features = [
-  { label: "Fast & Compact", value: "1" },
-  { label: "Search", value: "2" },
-  { label: "Edit", value: "3" },
-  { label: "Customize", value: "5" },
-  { label: "Compare", value: "6" },
-  { label: "AI-Powered Assistant", value: "7" },
-];
 
 export default function VideoPreview() {
-  const [selectedFeature, setSelectedFeature] = useState("1");
-
   return (
     <div className="mx-auto max-w-5xl">
       <div className="flex flex-col justify-center gap-8 sm:flex-row">
@@ -23,19 +11,16 @@ export default function VideoPreview() {
               muted
               loop
               playsInline
-              poster={`/assets/preview/${selectedFeature}.webp`}
+              poster={`/assets/preview/demo.webp`}
               className="h-auto w-full"
             >
               <source
-                src={`https://app.jsoncrack.com/assets/videos/p${selectedFeature}.mp4`}
+                src={`https://www.ritanshudokania.xyz/assets/videos/demo.mp4`}
                 type="video/mp4"
               />
               Your browser does not support the video tag.
             </video>
           </div>
-          {/* <p className="mt-2 text-center text-sm text-gray-500">
-            Previews are from the Premium version of the app
-          </p> */}
         </div>
       </div>
     </div>
