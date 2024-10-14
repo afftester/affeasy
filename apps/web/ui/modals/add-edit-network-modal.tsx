753e0d8bdf31e134f4349ec5a8fb3c1fcbc0853c
afftester <rsd1701@gmail.com>
2024-10-13 21:22:41 -0700

    Adding website url and name to planethowl data

--- apps/web/ui/modals/add-edit-network-modal.tsx
+++ apps/web/ui/modals/add-edit-network-modal.tsx
@@ -603,6 +603,41 @@ function AddEditNetworkModal({
                         htmlFor={`url-${randomIdx}`}
                         className="block text-sm font-medium text-gray-700"
                       >
+                        Website URL
+                      </label>
+                      <a
+                        href="https://docs.affeasy.link/quickstart/affiliate-networks/planethowl  "
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-gray-400 hover:text-gray-500"
+                      >
+                        <HelpCircle className="h-4 w-4" />
+                      </a>
+                    </div>
+                    <div className="relative mt-1 flex rounded-md shadow-sm">
+                      <input
+                        name="websiteID"
+                        id={`websiteID-${randomIdx}`}
+                        placeholder={
+                          "https://docs.affeasy.link/quickstart/affiliate-networks/commision-junction"
+                        }
+                        value={websiteId ?? ""}
+                        required
+                        autoComplete="off"
+                        onChange={(e) =>
+                          handleInputChange("websiteId", e.target.value)
+                        }
+                        className={`${"border-gray-300 text-gray-900 placeholder-gray-300 focus:border-gray-500 focus:ring-gray-500"} block w-full rounded-md focus:outline-none sm:text-sm`}
+                        aria-invalid="true"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <div className="flex items-center justify-between">
+                      <label
+                        htmlFor={`url-${randomIdx}`}
+                        className="block text-sm font-medium text-gray-700"
+                      >
                         API Key
                       </label>
                       <a
@@ -626,6 +661,41 @@ function AddEditNetworkModal({
                         autoComplete="off"
                         onChange={(e) =>
                           handleInputChange("partialApiKey", e.target.value)
+                        }
+                        className={`${"border-gray-300 text-gray-900 placeholder-gray-300 focus:border-gray-500 focus:ring-gray-500"} block w-full rounded-md focus:outline-none sm:text-sm`}
+                        aria-invalid="true"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <div className="flex items-center justify-between">
+                      <label
+                        htmlFor={`url-${randomIdx}`}
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Website Name
+                      </label>
+                      <a
+                        href="https://docs.affeasy.link/quickstart/affiliate-networks/planethowl"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-gray-400 hover:text-gray-500"
+                      >
+                        <HelpCircle className="h-4 w-4" />
+                      </a>
+                    </div>
+                    <div className="relative mt-1 flex rounded-md shadow-sm">
+                      <input
+                        name="accountId"
+                        id={`accountId-${randomIdx}`}
+                        placeholder={
+                          "https://docs.affeasy.link/quickstart/affiliate-networks/planethowl"
+                        }
+                        value={accountId ?? ""}
+                        required
+                        autoComplete="off"
+                        onChange={(e) =>
+                          handleInputChange("accountId", e.target.value)
                         }
                         className={`${"border-gray-300 text-gray-900 placeholder-gray-300 focus:border-gray-500 focus:ring-gray-500"} block w-full rounded-md focus:outline-none sm:text-sm`}
                         aria-invalid="true"
